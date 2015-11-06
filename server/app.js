31005fe75cdd27ae487f870427090c54877bfac4
User <user@Kates-MacBook-Pro.local>
2015-11-06 12:04:21 -0600

    initial commit

---
+++ server/app.js
@@ -0,0 +1,135 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/sql_lecture';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({expanded: true}));
+
+// Get all the people information
+app.get('/data', function(req,res){
+    var results = [];
+
+    //SQL Query > SELECT data from table
+    pg.connect(connectionString, function (err, client, done) {
+        var query = client.query("SELECT id, name, location, spirit_animal, address FROM people ORDER BY name ASC");
+
+        // Stream results back one row at a time, push into results array
+        query.on('row', function (row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            console.log(results);
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/find', function(req, res){
+
+        var results = [];
+        console.log("find this:", req.query.peopleSearch);
+
+        //SQL Query > SELECT data from table
+        pg.connect(connectionString, function (err, client, done) {
+            //var q = "SELECT name FROM people WHERE name LIKE '" + req.query.peopleSearch + "%'";
+            //console.log(q);
+            var query = client.query("SELECT * FROM people WHERE name LIKE '" + req.query.peopleSearch + "%'");
+
+            // Stream results back one row at a time, push into results array
+            query.on('row', function (row) {
+                results.push(row);
+            });
+
+            // After all data is returned, close connection and return results
+            query.on('end', function () {
+                client.end();
+                return res.json(results);
+            });
+
+            // Handle Errors
+            if (err) {
+                console.log("FIND ERROR", err);
+            }
+        });
+});
+//    pg.connect(connectionString, function (err, client) {
+//        client.query("SELECT name FROM people WHERE name LIKE " + req.query + "%"),
+//            function(err, result) {
+//                if(err) {
+//                    console.log("Error reading data: ", err);
+//                    res.send(false);
+//                }
+//                res.send(true);
+//            }
+//    });
+//});
+
+
+
+
+
+// Add a new person
+app.post('/data', function(req,res){
+    var addedPerson = {
+        "name" : req.body.peopleAdd,
+        "location" : req.body.locationAdd,
+        "spirit_animal" : req.body.spirit_animal,
+        "address" : req.body.address
+    };
+
+    pg.connect(connectionString, function (err, client) {
+
+        client.query("INSERT INTO people (name, location, spirit_animal, address) VALUES ($1, $2, $3, $4) RETURNING id",
+            [addedPerson.name, addedPerson.location, addedPerson.spirit_animal, addedPerson.address],
+            function(err, result) {
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                }
+
+                res.send(true);
+            });
+
+    });
+
+});
+
+app.delete('/data', function(req,res){
+    pg.connect(connectionString, function (err, client){
+
+        client.query("DELETE FROM people WHERE id = " + req.body.id),
+            function(err, result) {
+                console.log("This is Result: ", result);
+                if (err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                }
+                console.log("req.body.id : ", req.body.id);
+                res.send(true);
+
+            }
+    });
+});
+
+app.get("/*", function(req,res){
+    var file = req.params[0] || "/views/index.html";
+    res.sendFile(path.join(__dirname, "./public", file));
+});
+
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: ", app.get("port"));
+});
---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+   $("#search").submit(function(event){
+      event.preventDefault();
+      var values = {};
+
+      $.each($(this).serializeArray(), function(i, field){
+         values[field.name] = field.value;
+      });
+      if(values){
+         findPerson(values);
+      }
+      else{
+         getData();
+      }
+
+
+   });
+
+   $("#addSomeone").submit(addSomeone);
+   $("#peopleContainer").on('click', '.delete', function(){
+      deletePerson($(this).data("id"));
+      $(this).parent().remove();
+   });
+   getData();
+});
+
+
+function findPerson(values){
+   $.ajax({
+      type: "GET",
+      url: "/find",
+      data: values,
+      beforeSend: function(){
+         console.log(values);
+      },
+      success: function(data){
+         console.log(data);
+         updateDOM(data);
+      }
+   })
+}
+
+
+function getData(values){
+   $.ajax({
+      type: "GET",
+      url: "/data",
+      data: values,
+      success: function(data){
+         updateDOM(data);
+      }
+   })
+}
+
+function addSomeone(){
+   event.preventDefault();
+   var values = {};
+
+   $.each($(this).serializeArray(), function(i, field){
+      values[field.name] = field.value;
+   });
+
+   $.ajax({
+      type: "POST",
+      url: "/data",
+      data: values,
+      success: function(data){
+         getData();
+      }
+   });
+}
+
+function deletePerson(id){
+   var deletedId = {"id": id};
+
+   console.log("Meaningful Log: ", deletedId);
+
+   $.ajax({
+      type: "DELETE",
+      url: "/data",
+      data: deletedId,
+      success: function (data) {
+         console.log("This is the Delete Call: ", data);
+
+      }
+
+   });
+}
+
+function updateDOM(data){
+   $("#peopleContainer").empty();
+
+   for(var i = 0; i < data.length; i++){
+      var el = "<div class='well col-md-3'>" +
+                  "<p>" + data[i].name + "</p>" +
+                  "<p>" + data[i].location + "</p>" +
+                  "<p>" + data[i].spirit_animal + "</p>" +
+                  "<p>" + data[i].address + "</p>" +
+                  "<button class='delete btn btn-danger' data-id='" +
+                     data[i].id + "'>Delete</button>" +
+               "</div>";
+
+      $("#peopleContainer").append(el);
+   }
+}
